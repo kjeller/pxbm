0636dc50b55bb82680abc6f01a3567546aedccb3
Kjelle <karl.stralman@gmail.com>
2019-07-16 21:40:10 +0200

    + Added argument for reading .xbm files (uses the implemented decoder)  Run program as "pxbm <filepath> <R> <G> <B>" to  try it out.

--- src/main.c
+++ src/main.c
@@ -28,19 +28,22 @@ XBM test_9x4 = {
 };
 // ==============================
 
+// Run application as pxbm <filepath> <R> <G> <B>
 int main(int argc, char *argv[]) {
-	if(argc < 4) {
+	if(argc < 5) {
 		printf("Error: Missing arguments!");
 		return(1);
 	}
-	// TODO: Use filename from argv when argp is implemented
-	uint8_t *data = read_file("xbm/loink.xbm");
+	char *fname = malloc(256);
+	fname = argv[1]; // First argument should be filename
+	uint8_t *data = read_file(fname);
+	free(fname);
 
 	if(data) {
 		XBM *xptr = decode_xbm(data);
 		if(xptr) {
 			//Handle input as <R> <G> <B>
-			print_xbm(xptr, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+			print_xbm(xptr, atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
 		} else {
 			printf("Error: Could not decode file. Invalid data.");
 		}
