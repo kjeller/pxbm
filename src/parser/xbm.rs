e9d00d55403d2e39a1247bb47363a89ad7357119
kjeller <kallestralman@gmail.com>
2023-04-02 22:20:49 +0200

    Parse for xbm working
    Printing almost works, but is inverted atm

--- src/parser/xbm.rs
+++ src/parser/xbm.rs
@@ -1,53 +1,71 @@
 use super::PrintableFormat;
+use regex::Regex;
 
 pub struct Xbm {
-    data : Vec<u8>,
-    width : u8,
-    height : u8,
+    data: Vec<u8>,
+    width: u8,
+    height: u8,
 }
 
 impl Xbm {
     pub fn parse(input: &str) -> Xbm {
         print!("{}", input);
-        let xbm = Xbm { data: vec![], width: 0, height: 0};
 
-// TODO from C impl
-//   end = data + len;
+        let width = Regex::new(r"_width (\d+)")
+            .unwrap()
+            .captures(input)
+            .unwrap()[1]
+            .parse::<u8>()
+            .unwrap();
+        let height = Regex::new(r"_height (\d+)")
+            .unwrap()
+            .captures(input)
+            .unwrap()[1]
+            .parse::<u8>()
+            .unwrap();
 
-//   xptr->width = parse_str_int(data, len, "_width");
-//   xptr->height = parse_str_int(data, len, "_height");
+        let data: Vec<u8> = Regex::new(r"0[xX][0-9a-fA-F]+")
+            .unwrap()
+            .captures_iter(input)
+            .map(|f| {
+                let no_prefix = f[0].trim_start_matches("0x");
+                u8::from_str_radix(no_prefix, 16).unwrap()
+            })
+            .collect();
 
-//   next = memchr(data, '{', len); // get substring where data start
-//   if(next == NULL)
-//     return -1;
-
-//   ptr = next + 1;
-
-//   xptr->data = (char *)malloc(xptr->width * xptr->height);
-//   if(data == NULL)
-//     return -1;
-// 	long i = 0;
-//   while(ptr < end) {
-// 		if(ptr == NULL) {
-// 			break;
-// 		}
-//     while(*ptr != 'x') {
-//       ptr++;
-// 			if(ptr >= end) {
-// 				break;
-// 			}
-//     }
-//     char x = (char)strtol(ptr + 1, NULL, 16);
-//     xptr->data[i++] = x;
-//     ptr++;
-//   }
-
-        xbm
+        Xbm {
+            data: data,
+            width: width,
+            height: height,
+        }
     }
 }
 
 impl PrintableFormat for Xbm {
+    /**
+     * Prints xmb picture to console by reading bytewise (the bits) line by line
+     * and changing background color (ANSI) for every bit that is 'highlighted'.
+     */
     fn print(&self) {
-        print!("TODO");
+        // Width of bitmap in bytes
+        let w_bytes: i32 = ((self.width + 7) / 8) as i32;
+
+        for i in 0..self.height as i32 {
+            for j in 0..w_bytes as i32 {
+                let byte: u8 = self.data[(i * w_bytes + j) as usize];
+
+                for k in 0..8 as i32 {
+                    // Loop through the bits
+                    //for(int k = 0; k < 8; k++) {
+                    if byte & (1 << k) == 0 {
+                        print!("\x1b[48;2;{0};{1};{2}m  ", 255, 255, 255);
+                    } else {
+                        print!("\x1b[0m  ");
+                    }
+                }
+            }
+            println!();
+        }
+        print!("\x1b[0m  ");
     }
 }
