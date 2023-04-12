0d95e53c3210dfa6d057cdd80c14a8b9b1992b5f
kjeller <kallestralman@gmail.com>
2023-04-12 22:19:37 +0200

    Remove previous C implementation

--- include/netpbm/p5.h
+++
@@ -1,8 +0,0 @@
-#ifndef NETPBM_P5_H
-#define NETPBM_P5_H
-
-#include <stdint.h>
-
-int parse_netpbm_p5(NETPBM *pbm, uint8_t *seek);
-
-#endif // NETPBM_P5_H
--- include/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#include <stdint.h>
-
-uint8_t *read_file(char *fname, size_t *filesize);
-
-#endif // UTIL_H
